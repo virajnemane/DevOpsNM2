2fa0ed7db61e4ae2ea1a4286c940a5f4619ac242
Nilesh Indore <indorenilesh@gmail.com>
2020-06-20 20:56:16 +0530

    update...

--- LearnTERRAFORM/userdata.sh
+++ LearnTERRAFORM/userdata.sh
@@ -5,4 +5,4 @@ unzip terraform_0.12.23_linux_amd64.zip -d /home/ec2-user/bin
 mkdir /home/ec2-user/terraform
 cd /home/ec2-user/terraform
 git clone https://github.com/tciaindiadevops/LearnDevOps.git
-chown ec2-user:ec2-user /home/ec2-user/terraform -R
+chown ec2-user:ec2-user /home/ec2-user/terraform /home/ec2-user/bin -R
