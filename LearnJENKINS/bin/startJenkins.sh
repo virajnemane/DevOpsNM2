5eb6c636d5cd8002158491f88185e8d6a0f2a196
Nilesh Indore <indorenilesh@gmail.com>
2020-07-11 11:49:08 +0530

    update...

---
+++ LearnJENKINS/bin/startJenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ROOT_DIR=/local/apps/jenkins
+JENKINS_HOME=/local/apps/jenkins/home
+JENKINS_URL=/local/apps/jenkins
+
+for i in $JENKINS_HOME $ROOT_DIR/logs
+do
+  if [ ! -d $i ]
+  then
+    mkdir $i
+  fi
+done
+
+HTTP_PORT=9090
+JENKINS_ARGS="--webroot=/local/apps/jenkins/webroot --prefix=/jenkins --httpPort=$HTTP_PORT"
+
+export JENKINS_HOME JENKINS_ARGS JENKINS_URL
+mv $ROOT_DIR/logs/jenkins.log $ROOT_DIR/logs/jenkins_`date +%F-%H_%M`.log
+nohup `which java` -jar /local/apps/jenkins/jenkins.war $JENKINS_ARGS > $ROOT_DIR/logs/jenkins.log &
